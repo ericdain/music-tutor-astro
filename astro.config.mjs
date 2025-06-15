bd48a73937af4601ea027c302f34dcd435f0c3da
Eric Dain <eric.felker@gmail.com>
2025-06-15 16:17:58 -0400

    update configs to support sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,21 @@ import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  site: "https://ericdain.github.io",
-  base: "/music-tutor-astro",
-  integrations: [sentry(), spotlightjs()],
+    output: "static",
+    site: "https://ericdain.github.io",
+    base: "/music-tutor-astro",
+    integrations: [
+        sentry({
+            dsn: process.env.SENTRY_DSN,
+            tracesSampleRate: 0,
+            replaysSessionSampleRate: 0,
+            replaysOnErrorSampleRate: 0,
+            // Setting this option to true will send default PII data to Sentry.
+            // For example, automatic IP address collection on events
+            sendDefaultPii: true,
+            sourceMapsUploadOptions: {
+                project: "javascript-astro",
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+        },
+    }), spotlightjs()],
 });
