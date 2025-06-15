d55e977ff9df5cd051c88dbfb597d1a8b4f43536
Eric Dain <eric.felker@gmail.com>
2025-06-15 08:04:07 -0400

    :sparkles: init! :rocket:

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({// Replace 'your-github-username' with your actual GitHub username
+    site: "https://ericdain.github.io",
+    // Replace 'music-tutor-astro' with your repository name
+    base: "/music-tutor-astro",
+});
